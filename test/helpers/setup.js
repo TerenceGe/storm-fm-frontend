2da83bf92c320847476fdb20570306695331c428
Terence Ge <Terencegehui@yahoo.com>
2016-11-09 21:49:29 +0800

    setup unit test

--- test/helpers/setup.js
+++ test/helpers/setup.js
@@ -1,7 +1,14 @@
 /* global document, window */
 
 import Jsdom from 'jsdom'
+import hook from 'css-modules-require-hook'
+import chai from 'chai'
+import chaiEnzyme from 'chai-enzyme'
 
+chai.use(chaiEnzyme())
+hook({
+  generateScopedName: '[local]_[hash:base64:5]'
+})
 global.document = Jsdom.jsdom('<body><div id="app"></div></body>')
 global.window = document.defaultView
 global.navigator = window.navigator
