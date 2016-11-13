173a7cb9e2c958f916c4e61f283f6045c3baa836
Terence Ge <Terencegehui@yahoo.com>
2016-11-14 02:44:26 +0800

    migrate to webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
 const webpack = require('webpack')
 const fs = require('fs')
-const path = require('path')
+const { resolve } = require('path')
 const extend = require('extend')
+const BabiliPlugin = require("babili-webpack-plugin")
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
+const { getIfUtils, removeEmpty } = require('webpack-config-utils')
+
+const { ifProduction, ifNotProduction } = getIfUtils(process.env.NODE_ENV)
 
 const baseConfig = {
   resolve: {
@@ -16,32 +20,49 @@ const baseConfig = {
     chucks: false
   },
   module: {
-    preLoaders: [
+    rules: [
       {
         test: /\.jsx?$/,
         loader: 'eslint-loader',
+        enforce: 'pre',
         exclude: /node_modules/
-      }
-    ],
-    loaders: [
+      },
       {
         test: /\.css$/,
         include: /shared/,
-        loader: ExtractTextPlugin.extract(
-          'style-loader',
-          'css-loader?modules&sourceMap&importLoaders=1&localIdentName=[local]_[hash:base64:5]!postcss-loader'
-        )
+        loader: ExtractTextPlugin.extract({
+          fallbackLoader: 'style-loader',
+          loader: [
+            {
+              loader: 'css-loader',
+              query: {
+                modules: true,
+                sourceMap: true,
+                importLoaders: 1,
+                localIdentName: '[local]_[hash:base64:5]'
+              }
+            },
+            {
+              loader: 'postcss-loader'
+            }
+          ]
+        })
       },
       {
         test: /\.css$/,
         exclude: /shared/,
-        loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+        loader: ExtractTextPlugin.extract({
+          fallbackLoader: 'style-loader',
+          loader: {
+            loader: 'css-loader'
+          }
+        })
       },
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
-        query: { compact: false }
+        options: { compact: false }
       },
       {
         test: /\.json$/,
@@ -50,12 +71,20 @@ const baseConfig = {
       {
         test: /\.(ttf|eot|otf|svg|woff(2)?)(\?[a-z0-9]+)?$/,
         include: /shared\/resources\/fonts/,
-        loader: 'url-loader?limit=1024&name=fonts/[name].[ext]'
+        loader: 'url-loader',
+        options: {
+          limit: 1024,
+          name: 'fonts/[name].[ext]'
+        }
       },
       {
         test: /\.(ico|png|jpg|svg)$/,
         include: /shared\/resources\/images/,
-        loader: 'url-loader?limit=10240&name=images/[name].[ext]?v=[hash:base64:5]'
+        loader: 'url-loader',
+        options: {
+          limit: 10240,
+          name: 'images/[name].[ext]?v=[hash:base64:5]'
+        }
       },
       {
         test: /\.svg$/,
@@ -69,47 +98,30 @@ const baseConfig = {
   }
 }
 
-const nodeModules = {}
-
-fs.readdirSync('node_modules')
-.filter(file => !file.includes('.bin'))
-.forEach((module) => {
-  nodeModules[module] = `commonjs ${ module }`
-})
-
-delete nodeModules['normalize.css']
-
-const devConfig = extend(true, {}, baseConfig, {
-  context: path.join(__dirname, './client'),
-  entry: {
-    jsx: './index.js',
-    vendor: [
-      'react',
-      'react-dom',
-      'react-redux',
-      'react-router',
-      'react-router-redux',
-      'redux',
-      'redux-actions',
-      'intl',
-      'intl/locale-data/jsonp/en.js',
-      'intl/locale-data/jsonp/zh.js',
-      'react-intl',
-      'react-cookie'
-    ]
-  },
+const clientConfig = extend(true, {}, baseConfig, {
+  context: resolve('client'),
+  entry: './index.js',
   output: {
-    path: path.join(__dirname, './static'),
-    filename: 'bundle.js',
+    path: resolve('static'),
+    filename: ifProduction('bundle.js?v=[hash]', 'bundle.js'),
     publicPath: '/'
   },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js'),
-    new webpack.DefinePlugin({
-      'process.env': { NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development') }
+  plugins: removeEmpty([
+    new BabiliPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
     }),
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'production') }
+    }),
+    ifProduction(new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false },
+      output: { comments: false },
+      sourceMap: false
+    })),
     new CleanWebpackPlugin(['static'], {
-      root: path.join(__dirname, './'),
+      root: resolve('./'),
       verbose: true,
       compress: { warnings: false },
       output: { comments: false }
@@ -121,97 +133,72 @@ const devConfig = extend(true, {}, baseConfig, {
       appMountId: 'app',
       mobile: true
     }),
-    new ExtractTextPlugin('bundle.css', { allChunks: true })
-  ],
+    new ExtractTextPlugin({
+      filename: ifProduction('bundle.css?v=[hash]', 'bundle.css'),
+      disable: false,
+      allChunks: true
+    })
+  ]),
   devServer: {
     contentBase: './client',
     hot: true
   }
 })
 
-const prodConfig = [
-  extend(true, {}, baseConfig, {
-    context: path.join(__dirname, './client'),
-    entry: {
-      bundle: './index.js',
-      vendor: [
-        'react',
-        'react-dom',
-        'react-redux',
-        'react-router',
-        'react-router-redux',
-        'redux',
-        'redux-actions',
-        'intl',
-        'intl/locale-data/jsonp/en.js',
-        'intl/locale-data/jsonp/zh.js',
-        'react-intl',
-        'react-cookie'
-      ]
-    },
-    output: {
-      path: path.join(__dirname, './static'),
-      filename: 'bundle.js?v=[hash]',
-      publicPath: '/'
-    },
-    target: 'web',
-    plugins: [
-      new webpack.ExtendedAPIPlugin(),
-      new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js?v=[hash]'),
-      new webpack.DefinePlugin({
-        'process.env': { NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'production') }
-      }),
-      new webpack.optimize.UglifyJsPlugin({
-        compress: { warnings: false },
-        output: { comments: false }
-      }),
-      new CleanWebpackPlugin(['static'], {
-        root: path.join(__dirname, './'),
-        verbose: true,
-        compress: { warnings: false },
-        output: { comments: false }
-      }),
-      new HtmlWebpackPlugin({
-        inject: false,
-        template: 'index.html',
-        title: 'Storm FM',
-        appMountId: 'app',
-        mobile: true
-      }),
-      new ExtractTextPlugin('bundle.css?v=[hash]', { allChunks: true }),
-    ]
-  }),
-  extend(true, {}, baseConfig, {
-    context: path.join(__dirname, './server'),
-    entry: './index.js',
-    output: {
-      path: path.join(__dirname, './static'),
-      filename: 'app.js',
-      libraryTarget: 'commonjs2',
-      publicPath: '/'
-    },
-    externals: nodeModules,
-    target: 'node',
-    node: {
-      console: false,
-      global: false,
-      process: false,
-      Buffer: false,
-      __filename: false,
-      __dirname: false
-    },
-    plugins: [
-      new webpack.ExtendedAPIPlugin(),
-      new webpack.DefinePlugin({
-        'process.env': { NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'production') }
-      }),
-      new webpack.optimize.UglifyJsPlugin({
-        compress: { warnings: false },
-        output: { comments: false }
-      }),
-      new ExtractTextPlugin('bundle.css', { allChunks: true })
-    ]
+const nodeModules = {}
+
+fs.readdirSync('node_modules')
+      .filter(file => !file.includes('.bin'))
+  .forEach((module) => {
+    nodeModules[module] = `commonjs ${ module }`
   })
+
+delete nodeModules['normalize.css']
+
+const serverConfig = extend(true, {}, baseConfig, {
+  context: resolve('server'),
+  entry: './index.js',
+  output: {
+    path: resolve('static'),
+    filename: 'app.js',
+    libraryTarget: 'commonjs2',
+    publicPath: '/'
+  },
+  externals: nodeModules,
+  target: 'node',
+  node: {
+    console: false,
+    global: false,
+    process: false,
+    Buffer: false,
+    __filename: false,
+    __dirname: false
+  },
+  plugins: [
+    new BabiliPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+    new webpack.ExtendedAPIPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'production') }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false },
+      output: { comments: false }
+    }),
+    new ExtractTextPlugin({
+      filename: 'bundle.css',
+      disable: false,
+      allChunks: true
+    })
+  ]
+})
+
+const prodConfig = [
+  clientConfig,
+  serverConfig
 ]
 
-module.exports = process.env.NODE_ENV === 'development' ? devConfig : prodConfig
+module.exports = process.env.NODE_ENV === 'development' ? clientConfig : prodConfig
